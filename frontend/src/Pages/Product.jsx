7cade2ee3887d3f69ae252a72c4bd0e865f3c03d
domivbhh <hariharanvbit123@gmail.com>
2024-07-04 13:58:54 +0530

    updated img2

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -36,7 +36,7 @@ const Product = () => {
           </div>
           <img
             src="https://www.jimphicdesigns.com/downloads/imgs-mockup/green-arrow-outline-pointing-down-animation.gif"
-            className="w-20 h-20 rounded-full -mt-[10%] ml-[87%]"/>
+            className="w-20 h-20 rounded-full -mt-[10%] ml-[89%]"/>
         </div>
 
         <div className="my-24">
